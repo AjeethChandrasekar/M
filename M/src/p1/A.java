42aec4afb95fadb5fe59c88f67b7ac383bd5c686
AjeethChandrasekar <109085169+AjeethChandrasekar@users.noreply.github.com>
2022-07-14 13:30:45 +0530

    Arrays value changed

--- M/src/p1/A.java
+++ M/src/p1/A.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class A {
 public static void main(String[] args) {
-	int a[] = {5,2,4,1,3};
+	int a[] = {10,50,1,65,43};
 	Arrays.sort(a);
 	for (int i : a) {
 		System.out.println(i);
